4ee95876213fd5b34db7cf839eef3eef29a1cbd9
KOn4OHOCTu <59607285+Nikaiai@users.noreply.github.com>
2023-04-16 17:44:57 +0300

    Update PrZ_15_5.py

--- PrZ_15/PrZ_15_5.py
+++ PrZ_15/PrZ_15_5.py
@@ -1,29 +1,29 @@
 import sqlite3
 with sqlite3.connect('Аптека.db') as con:
     cur = con.cursor()
-#1, 11
+ #1, 11
     #cur.execute("DELETE FROM Больничные_листы WHERE id_сотрудника = (SELECT DISTINCT id FROM Анкеты WHERE имя = 'Иван')")
-#2
+ #2
     #cur.execute("DELETE FROM Больничные_листы WHERE id_сотрудника = (SELECT DISTINCT id FROM Анкеты WHERE фамилия = 'Петров')")
-#3
+ #3
     #cur.execute("DELETE FROM Больничные_листы WHERE id_сотрудника = (SELECT id FROM Анкеты WHERE должность = 'Менеджер')")
-#4
+ #4
     #cur.execute("DELETE FROM Больничные_листы WHERE id_сотрудника = (SELECT id FROM Анкеты WHERE отдел = 'Продаж')")
-#5
+ #5
     #cur.execute("DELETE FROM Больничные_листы WHERE id_сотрудника = (SELECT id FROM Анкеты WHERE пол = 'женский')")
-#6
-    ####cur.execute("DELETE FROM Больничные_листы WHERE id_сотрудника = (SELECT id FROM Анкеты WHERE julianday(дата_рождения) - julianday('2023-04-10') < -18000)")
-#7
+ #6, 15
+    ##cur.execute("DELETE FROM Больничные_листы WHERE id_сотрудника = (SELECT id FROM Анкеты WHERE julianday(дата_рождения) - julianday('2023-04-10') < -18000)")
+ #7
     #cur.execute("DELETE FROM Больничные_листы WHERE оплачен = FALSE")
-#8
+ #8
     #cur.execute("DELETE FROM Больничные_листы WHERE дата_окончания < '2023-04-14'")
-#9
+ #9
     #cur.execute("DELETE FROM Больничные_листы WHERE дата_начала > '2023-01-01'")
-#10
+ #10
     #cur.execute("DELETE FROM Больничные_листы WHERE дата_окончания > '2023-01-01'")
-#12    
-    ####cur.execute("DELETE FROM Больничные_листы WHERE id_сотрудника = (SELECT id FROM Анкеты WHERE фамилия LIKE 'С%')")
-#13
-    #cur.execute("DELETE FROM Больничные_листы WHERE id_сотрудника = (SELECT id FROM Анкеты WHERE должность = 'Менеджер' AND оплачен = FALSE)")
-#14
-    cur.execute("DELETE FROM Больничные_листы WHERE id_сотрудника = (SELECT id FROM Анкеты WHERE отдел = 'IT' AND дата_окончания > '2023-01-01')")
+ #12    
+    ##cur.execute("DELETE FROM Больничные_листы WHERE id_сотрудника = (SELECT id FROM Анкеты WHERE фамилия LIKE 'С%')")
+ #13
+    ##cur.execute("DELETE FROM Больничные_листы WHERE id_сотрудника = (SELECT id FROM Анкеты WHERE должность = 'Менеджер' AND оплачен = FALSE)")
+ #14
+    ##cur.execute("DELETE FROM Больничные_листы WHERE id_сотрудника = (SELECT id FROM Анкеты WHERE отдел = 'IT' AND дата_окончания > '2023-01-01')")
