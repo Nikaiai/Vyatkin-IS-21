ba484194f428353eef80b45618a8ca00aaf71136
KOn4OHOCTu <59607285+Nikaiai@users.noreply.github.com>
2023-05-22 21:13:39 +0300

    add comm

--- PrZ_16/PrZ_16.py
+++ PrZ_16/PrZ_16.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
 from figures import circle_perimeter, circle_area
 from figures import square_area, square_perimeter
 from figures import triangle_area, triangle_perimeter
